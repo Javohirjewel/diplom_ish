9e4d1f3a07087c1bbd6f3a371785956ead49ae7f
Javohirjewel <shdomonovjavohir56@gmail.com>
2023-04-27 17:33:39 +0500

    first commit

---
+++ my-app/src/components/App/App.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import {useState, useEffect} from 'react';
+import './App.css';
+import Header from '../Header/Header'
+import SearchPanel  from '../SearchPanel/Searchpanel';
+import Main  from '../Main/main';
+
+
+function App() {
+  const [darkmode, setDarkmode] = useState(false)
+  const [data, setData] = useState(null)
+  const[text, setText] = useState('')
+  const[filter, setFilter] = useState("Barchasi")
+  const[dirfilter, setDirfilter] = useState("Barchasi")
+  
+  const onCahngeTheme = () => {
+    setDarkmode(!darkmode)
+  }
+
+const inpSearch = (e) => {
+  setText(e.target.value)
+}
+
+async function getData(){
+  await fetch('https://6440e8a9277c695770cad586--incandescent-belekoy-d804da.netlify.app/db.json')
+     .then(res => res.json())
+     .then(setData)
+     console.log(data)
+ }
+
+const searchData = () => {
+  if(text === ''){
+    return getFilter()
+  }else{
+    return getFilter().filter(el => el.name.toLowerCase().includes(text.toLowerCase()))
+  }
+  
+}
+
+useEffect(() => {
+  getData()
+  searchData()
+
+
+}, [])
+
+
+const getFilter = () =>{
+  switch (filter) {
+    case "Koson": return data.filter(el => el.region === 'Koson'); 
+    case "Qarshi": return data.filter(el => el.region === 'Qarshi'); 
+    case "Shahrisabz": return data.filter(el => el.region === 'Shahrisabz'); 
+    case "Yakkabog'": return data.filter(el => el.region === "Yakkabog'");
+    default: return data;
+  }
+}
+
+
+const getDirfilter = (qiy) =>{
+  console.log(qiy)
+  switch (qiy) {
+    case "IT markazlari": return getFilter().filter(el => el.direction === 'IT'); 
+    case "Abituriyentlarni tayyorlash kurslari": return getFilter().filter(el => el.direction === 'Abituriyent'); 
+    case "Kasb Hunar o'rgatish markazlari": return getFilter().filter(el => el.direction === 'Service'); 
+    case "Bolalar uchun tayyorlov kurslari": return getFilter().filter(el => el.direction === "Kids'");
+    default: return data;
+  }
+}
+const setFilterData = (select) => {
+  setFilter(select)
+}
+// getDirfilter()
+getFilter()
+
+  return (
+    <div className="App">
+       <Header darkmode = {darkmode} onCahngeTheme = {onCahngeTheme} setFilterData={setFilterData}/>
+       <SearchPanel darkmode = {darkmode} inpSearch = {inpSearch} text = {text} setText = {setText} />
+       <Main darkmode = {darkmode} Filter = {getDirfilter} />
+    </div>
+  );
+}
+export default App;
---
+++ my-app/src/components/Header/Header.js
@@ -0,0 +1,88 @@
+import rasm from '../img/iconMarkaz.webp'
+import './Header.css'
+import React from 'react';
+import {useState } from 'react';
+function Header(props) {
+  let {darkmode, onCahngeTheme, setFilterData} = props
+
+  const [select, setSelect] = useState("Tuman bo'yicha tanlash ?")
+  const [isActive, setIsActive] = useState(false)
+  const [isLanguageActive, setisLanguageActive] = useState(false)
+  const [language, setLanguage] = useState('UZB')
+
+  const onChangeSelect = (e) => {
+    setSelect(e.target.innerText)
+      setIsActive(false)
+      setFilterData(e.target.innerText)
+  }
+  const onChangeLanguage = (e) => {
+    setLanguage(e.target.innerText)
+    setisLanguageActive(false)
+  }
+
+    return (
+      <div className={darkmode ? "header darktheme " : " header"}>
+        <div className='header__container'>
+          <div className = 'header__logo'>
+            <img src={rasm} alt = 'Logo'/>
+          </div>
+          <div className = 'header__navbar'>
+              <div className={darkmode ? 'dark phone' : "phone"}>
+                <img src={"https://upload.wikimedia.org/wikipedia/commons/9/90/Telephone_receiver.svg"} alt = "s"></img>
+                93 339 08 48
+              </div>
+              <div className = 'region'>
+                <div onClick={() => setIsActive(!isActive)}  className='region__select'> {select}
+                <div className='svg'>
+                    <svg width="10" height="10" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path fill-rule="evenodd" clip-rule="evenodd" d="M8.45 0.450001L5 3.9L1.55 0.450001L0.5 1.5L5 6L9.5 1.5L8.45 0.450001Z" fill="black"/>
+                  </svg>
+                </div>
+                </div>
+                <div onClick={(e) => onChangeSelect(e)}  className={isActive ? 'region__drop ' : 'hide'}>
+                    <div className='drop__link' >
+                      <span>Barchasi</span>
+                    </div>
+                    <div className='drop__link' >
+                      <span>Qarshi</span>
+                    </div> 
+                    <div className='drop__link' >
+                        <span>Koson</span>
+                    </div> 
+                    <div className='drop__link' >
+                        <span>Shahrisabz</span>
+                    </div> 
+                    <div className='drop__link' >
+                        <span>Yakkabog'</span>
+                    </div> 
+                </div>
+              </div>
+              {/* <div className='language'>
+            <div className='language__select' onClick={() => setisLanguageActive(!isLanguageActive)} >
+              {language} 
+            </div>
+            <div onClick={(e) => onChangeLanguage(e)}  className={isLanguageActive ? 'language__drop' : 'hide'}>
+              <div className='drop__language'>UZB</div>
+              <div className='drop__language'>РУС</div>
+            </div>
+              </div> */}
+              <div onClick={onCahngeTheme} className="theme">
+                {!darkmode ?
+                <div className='lightIcon'>
+                  <svg width="30" height="30" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M13.5532 13.815C9.66857 13.815 6.51929 10.9278 6.51929 7.36821C6.51929 6.0253 6.96679 4.78158 7.73143 3.75C4.69036 4.69515 2.5 7.33122 2.5 10.4381C2.5 14.3385 5.94929 17.5 10.2036 17.5C13.5929 17.5 16.4696 15.4932 17.5 12.7045C16.375 13.4048 15.0161 13.815 13.5532 13.815Z" fill="white" stroke="#111517" stroke-width="1.25"/>
+                  </svg></div> :
+                  <div className='darkIcon'>
+                  <svg width="30" height="30" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M13.5532 13.815C9.66857 13.815 6.51929 10.9278 6.51929 7.36821C6.51929 6.0253 6.96679 4.78158 7.73143 3.75C4.69036 4.69515 2.5 7.33122 2.5 10.4381C2.5 14.3385 5.94929 17.5 10.2036 17.5C13.5929 17.5 16.4696 15.4932 17.5 12.7045C16.375 13.4048 15.0161 13.815 13.5532 13.815Z" fill="white"/>
+                  </svg>
+                </div>}
+                <p className={darkmode ? "dark theme__button" : "theme__button"}>Dark mode</p>
+              </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Header;
